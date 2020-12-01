f9920e76975f5fc2ffbee66174e3778afc9cd23a
David Belgrod <davidbelgrod@davids-air.fios-router.home>
2020-12-01 14:10:07 -0500

    clock switching off windows header

--- Broadmark/Algorithms/Core/Clock.h
+++ Broadmark/Algorithms/Core/Clock.h
@@ -2,8 +2,12 @@
 
 #pragma once
 
-
+#if defined(WIN32) || defined(_WIN32)
 #include <Windows.h>
+#else  
+#include <time.h>
+#endif 
+
 #include "Core/Results.h"
 
 
@@ -11,24 +15,41 @@
 // This is one of the few windows-only parts of the code
 class Clock {
 private:
+	#if defined(WIN32) || defined(_WIN32)
 	LARGE_INTEGER m_freq;
 	LARGE_INTEGER m_reference;
+	#else
+	struct timespec m_freq;
+	struct timespec m_reference;
+	#endif
 
 public:
 	Clock() {
+		#if defined(WIN32) || defined(_WIN32)
 		QueryPerformanceFrequency(&m_freq);
+		#else
+		clock_gettime(CLOCK_REALTIME, &m_freq);
+		#endif
 		reset();
 	}
 
 	void reset() {
+		#if defined(WIN32) || defined(_WIN32)
 		QueryPerformanceCounter(&m_reference);
+		#else
+		clock_gettime(CLOCK_REALTIME, &m_reference);
+		#endif
 	}
 	double getAndReset() {
+		#if defined(WIN32) || defined(_WIN32)
 		const LARGE_INTEGER reference = m_reference;
 		QueryPerformanceCounter(&m_reference);
-
 		// No need to bloat log files with smaller than 10^{-6} values
 		const double timeInSeconds = (double)((m_reference.QuadPart - reference.QuadPart) / (double)m_freq.QuadPart);
+		#else
+		const struct timespec reference = m_reference;
+		clock_gettime(CLOCK_REALTIME, &m_reference);
+		const double timeInSeconds = (double)((m_reference.tv_sec - reference.tv_sec) / (double)m_freq.tv_sec);
 		return timeInSeconds > 0.000001f ? timeInSeconds : 0;
 	}
 };
